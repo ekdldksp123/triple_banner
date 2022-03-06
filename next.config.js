e2dedf1363f19dc5267688b5c27d2e705f00dd70
ekdldksp123 <ekdldksp123@naver.com>
2022-03-06 16:24:39 +0900

    :tada::construction_worker::heavy_plus_sign::wrench::memo: project setting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? 'https://ekdldksp123.github.io/triple_banner/' : '',
+  staticPageGenerationTimeout: 120,
+  images: {
+    loader: 'akamai',
+    path: '',
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes : [ 640 , 750 , 828 , 1080 , 1200 , 1920 , 2048 , 3840 ],
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
