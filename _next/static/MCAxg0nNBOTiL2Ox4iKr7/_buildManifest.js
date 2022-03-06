bcb7c59951b15d1edb85621a7f9153c5968d4903
ekdldksp123 <64599394+ekdldksp123@users.noreply.github.com>
2022-03-06 15:05:24 +0000

    Deploying to gh-pages from @ ekdldksp123/triple_banner@5553dbedb11015587f7636b70fcf4e3e9492289e 🚀

---
+++ _next/static/MCAxg0nNBOTiL2Ox4iKr7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/521-b89ac072a7ab30a5.js","static/chunks/pages/index-a05beb6ebb43aca7.js"],"/_error":["static/chunks/pages/_error-8022dacb1937fc7a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
