fbf0024c407a56aef48e3b611dccf4bfb607ffe2
ekdldksp123 <ekdldksp123@naver.com>
2022-03-13 18:40:26 +0900

    :hammer: edit image path

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
   staticPageGenerationTimeout: 120,
   images: {
     loader: 'akamai',
-    path: 'triple_banner/',
+    path: 'triple_banner',
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [1200, 1920, 2048, 3840],
   },
