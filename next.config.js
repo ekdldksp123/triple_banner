03a780f0976719a8fdeca7378d689954574a88e8
ekdldksp123 <ekdldksp123@naver.com>
2022-03-13 18:57:17 +0900

    :hammer: edit next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
 /** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === 'production'
+const productionUrl = 'https://ekdldksp123.github.io'
+const productionPath = '/triple_banner'
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? 'https://ekdldksp123.github.io/triple_banner/' : '',
   staticPageGenerationTimeout: 120,
   images: {
     loader: 'akamai',
-    path: 'triple_banner',
+    path: isProd ? productionUrl : 'http://localhost:3000',
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [1200, 1920, 2048, 3840],
   },
+  assetPrefix: isProd ? productionPath : '',
+  pageExtentions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx']
 }
 
 module.exports = nextConfig
